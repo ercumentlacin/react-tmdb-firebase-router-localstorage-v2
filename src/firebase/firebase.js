df517f7897d316772253e9b07ed59b9afadeb232
ercumentlacin <forever.ducale@gmail.com>
2021-02-15 22:38:22 +0300

    create firebase config file

---
+++ src/firebase/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: process.env.REACT_API_KEY,
+    authDomain: process.env.REACT_API_AUTH_DOMAIN,
+    projectId: process.env.REACT_API_PROJECT_ID,
+    storageBucket: process.env.REACT_API_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_API_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_API_APP_ID
+  };
